8d0b07d6bafca8a4e14a563a270bd2c8f0e95982
fatima rahimi <iamfatimarahimi@gmail.com>
2023-10-02 21:17:05 +0330

    Added icon from API

--- src/app.js
+++ src/app.js
@@ -13,21 +13,25 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minuets}`
 }
 let apiKey = `57a9ed06d8a8a6f501b0cd3f20bdc49a`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=57a9ed06d8a8a6f501b0cd3f20bdc49a&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=57a9ed06d8a8a6f501b0cd3f20bdc49a&units=metric`;
+
 function displayTempreture(response){
-    console.log(response.data);
     let temperature = document.querySelector('#temperature');
     let cityElement = document.querySelector('#city');
     let descriptionEelment = document.querySelector('#description');
     let humidityElement = document.querySelector('#humidity');
     let windElement = document.querySelector('#wind');
     let dateElement = document.querySelector('#date');
+    let iconElement = document.querySelector('#icon');
     temperature.innerHTML =Math.round (response.data.main.temp);
     cityElement.innerHTML =response.data.name;
     descriptionEelment.innerHTML  = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML =Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt*1000)
+    dateElement.innerHTML = formatDate(response.data.dt*1000);
+    iconElement.setAttribute("src" , `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt" , response.data.weather[0].description)
 };
 
  axios.get(apiUrl).then(displayTempreture);
